76bea7df82de360b78beb83649e8f9c840045ad2
apenasrr <apenasrr@gmail.com>
2020-11-11 22:55:14 -0300

    mass_videojoin.py | Deploy feature of Resuming the process 'videos reencode' after previous interruption

--- mass_videojoin.py
+++ mass_videojoin.py
@@ -15,6 +15,8 @@ from config_handler import handle_config_file
 import unidecode
 import natsort
 import glob
+import sys
+import hashlib
 
 
 def logging_config():
@@ -276,58 +278,217 @@ def exclude_all_files_from_folder(path_folder):
             os.remove(i)
     
 
-def make_reencode(df):
+def make_reencode(path_file_report):
 
-    folder_script_path = get_folder_script_path()
-    path_folder_encoded = os.path.join(folder_script_path, 'videos_encoded')
-    exclude_all_files_from_folder(path_folder_encoded)
-    
-    # exclude_folder_videos_encoded()
-
-    df['file_folder_origin'] = df['file_folder']
-    df['file_name_origin'] = df['file_name']
-    df['file_size_origin'] = df['file_size']
-    df['video_resolution_origin'] = df['video_resolution']
-    mask_df_to_reencode = ~df['video_resolution_to_change'].isna()
-    df_to_reencode = df.loc[mask_df_to_reencode, :]
-    
-    folder_script_path = get_folder_script_path()
-    path_folder_dest = os.path.join(folder_script_path, 'videos_encoded')
-    for index, row in df_to_reencode.iterrows():
-        size_width, size_height = row['video_resolution_to_change'].split('x')
+    def get_file_name_dest(file_folder_origin, file_name_origin):
+        """
+        Create a hashed file name dest. 
+        Template: reencode_{file_name_origin}_{hash}.mp4"
+        """
+        file_folder_origin_encode = file_folder_origin.encode('utf-8')
+        hash = hashlib.md5(file_folder_origin_encode).hexdigest()[:5]
+        file_name_origin_without_extension = \
+            os.path.splitext(file_name_origin)[0]
+        file_name_dest = 'reencode_' + \
+                         file_name_origin_without_extension + '_' + \
+                         hash + '.mp4'
+        return file_name_dest
         
-        path_file_origin = os.path.join(row['file_folder_origin'], 
-                                        row['file_name_origin'])
-        print(path_file_origin)
-        # path_folder_dest = r'videos_encoded'
-        path_file_name_dest = str(index) + '.mp4'
+    def create_backup_columns(df):
+        
+        # Ensure creation of column bellow
+        list_backup_columns = ('file_folder', 'file_name', 'file_size', 
+                               'video_resolution')
+        
+        for column_name in list_backup_columns:
+            new_column_name = column_name + '_origin'
+            if new_column_name not in df.columns:
+                df[new_column_name] = df[column_name]
+        return df
+
+    def get_next_video_to_reencode(path_file_report):
+        
+        try:
+            df = pd.read_excel(path_file_report)
+        except Exception as e:
+            logging.error(f"Can't open file: {path_file_report}")
+            logging.error(e)
+        mask_df_to_reencode = ~df['video_resolution_to_change'].isna()
+        mask_df_reencode_not_done = df['reencode_done'].isin([0])
+        mask_df_to_reencode =  mask_df_to_reencode & mask_df_reencode_not_done
+        
+        df_to_reencode = df.loc[mask_df_to_reencode, :]
+        
+        qt_videos_to_reencode = df_to_reencode.shape[0]
+        if qt_videos_to_reencode == 0:
+            return False
+        
+        df_to_reencode = df_to_reencode.reset_index(drop=True)
+        dict_first_line = df_to_reencode.loc[0,:]
+        return dict_first_line
+
+    def reencode_video(dict_):
+
+        try:
+            video_resolution_to_change = dict_['video_resolution_to_change']
+            size_width, size_height = \
+                video_resolution_to_change.split('x')
+        except Exception as e:
+            path_file_origin = os.path.join(dict_['file_folder_origin'], 
+                                            dict_['file_name_origin'])            
+            logging.error(f'Parse. Column video_resolution_to_change: ' + 
+                          f'"{video_resolution_to_change}". '
+                          f'File:\n{path_file_origin}')
+            return False
+            
+        file_folder_origin = dict_['file_folder_origin']
+        file_name_origin = dict_['file_name_origin']
+        path_file_origin = os.path.join(file_folder_origin, 
+                                        file_name_origin)
+        
+        file_name_dest = get_file_name_dest(file_folder_origin, 
+                                            file_name_origin)
+        
+        path_folder_dest = os.path.join(folder_script_path, 'videos_encoded')
         path_file_dest = os.path.join(path_folder_dest,
-                                      path_file_name_dest)
+                                      file_name_dest)
         
-        # reencode 
-        # input path_folder_dest in column file_folder
-        df.loc[index, 'file_folder'] = os.path.abspath(path_folder_dest)
-        # input path_file_name_dest in column file_name
-        df.loc[index, 'file_name'] = path_file_name_dest
         
+        # Make video reencode 
+        logging.info(f'Start reencode: {path_file_origin}')
+
         change_width_height_mp4(path_file_origin, size_height, 
                                 size_width, path_file_dest)
+
+    def ask_for_delete_old_videos_encode(path_folder_encoded):
+    
+        for root, dirs, files in os.walk(path_folder_encoded):
+            
+            list_file_name_encoded = list(files)
         
         
+        len_list_file_name_encoded = len(list_file_name_encoded)
+        if len_list_file_name_encoded > 0:
+            print('\nThere is files in videos_encoded folder.\n' + \
+                  'Do you wish delete them?')
+            answer_delete = input('(None for yes) Answer: ')
+            
+            if answer_delete == '':
+                confirm_delete = input('\nType Enter to delete all ' + \
+                                       'video_encoded files.')
+                if confirm_delete == '':
+                    exclude_all_files_from_folder(path_folder_encoded)
+                else:
+                    pass
+    
+    def update_file_report(path_file_report, dict_video_data):
+
+        try:
+            df = pd.read_excel(path_file_report)
+        except Exception as e:
+            logging.error(f"Can't open file: {path_file_report}")
+            logging.error(e)
+
+        # find path_folder_dest and path_file_dest
+        file_folder_origin = dict_video_data['file_folder_origin']
+        file_name_origin = dict_video_data['file_name_origin']
+        path_file_origin = os.path.join(file_folder_origin, 
+                                        file_name_origin)
+        file_name_dest = get_file_name_dest(file_folder_origin, 
+                                            file_name_origin)
+        path_folder_dest = os.path.join(folder_script_path, \
+                                        'videos_encoded')
+        path_file_dest = os.path.join(path_folder_dest,
+                                      file_name_dest)
+        
+        # Check if file_name_dest exist
+        test_file_exist = os.path.isfile(path_file_dest)
+        if test_file_exist is False:
+            logging.error(f'After reencode, when update, '+
+                          f'reencoded file not exist:\n{path_file_dest}')
+            sys.exit()
+        
+        # locate index video in df
+        mask_file_folder = df['file_folder'].isin([file_folder_origin])
+        mask_file_name = df['file_name_origin'].isin([file_name_origin])
+        mask_line = mask_file_folder & mask_file_name
+        df_filter = df.loc[mask_line, :]
+        len_df_filter = len(df_filter)
+        if len_df_filter != 1:
+            logging.error(f'Need 1. Find {len_df_filter} line for ' + 
+                          f'video: {path_file_origin}')
+            sys.exit()    
+        index_video = df_filter.index
+        
+        # update df
+        df.loc[index_video, 'file_folder'] = \
+            os.path.abspath(path_folder_dest)
+        df.loc[index_video, 'file_name'] = file_name_dest
+
+    
         file_size = os.stat(path_file_dest).st_size
-        df.loc[index, 'file_size'] = file_size
-        df.loc[index, 'video_resolution'] = row['video_resolution_to_change']
+        video_resolution_to_change = \
+            dict_video_data['video_resolution_to_change']
+            
+        df.loc[index_video, 'file_size'] = file_size
+        df.loc[index_video, 'video_resolution'] = \
+            video_resolution_to_change
 
         # from encoded video get video metadata
         metadata = get_video_details(path_file_dest)
         # register video metadata
-        df.loc[index, 'bitrate'] = metadata['bitrate']
-        df.loc[index, 'video_bitrate'] = metadata['video']['bitrate']
-        df.loc[index, 'video_codec'] = metadata['video']['codec']
-        df.loc[index, 'audio_codec'] = metadata['audio']['codec']
-        df.loc[index, 'audio_bitrate']  = metadata['audio']['bitrate']
-        df.loc[index, 'duration'] = metadata['duration']
+        df.loc[index_video, 'bitrate'] = metadata['bitrate']
+        df.loc[index_video, 'video_bitrate'] = metadata['video']['bitrate']
+        df.loc[index_video, 'video_codec'] = metadata['video']['codec']
+        df.loc[index_video, 'audio_codec'] = metadata['audio']['codec']
+        df.loc[index_video, 'audio_bitrate']  = metadata['audio']['bitrate']
+        df.loc[index_video, 'duration'] = metadata['duration']
+        df.loc[index_video, 'reencode_done'] = 1
+        return df
         
+    folder_script_path = get_folder_script_path()
+    path_folder_encoded = os.path.join(folder_script_path, 'videos_encoded')
+    
+    df = pd.read_excel(path_file_report)
+    df = create_backup_columns(df)
+
+    ask_for_delete_old_videos_encode(path_folder_encoded)
+    
+    # TODO Test if all files mark as '1' in column reencode_done, 
+    # TODO  are in folder videos_encoded
+    
+    # Ensure creation of column 'reencode_done'. Pseudobolean 1 or 0    
+    if 'reencode_done' not in df.columns:
+        df['reencode_done'] = 0
+
+    # Save reports
+    df.to_excel(path_file_report, index=False)
+    create_report_backup(df=df, path_file_report=path_file_report, 
+                         tag='reencode')
+   
+    need_reencode = True
+    while need_reencode:
+        return_next_video_to_reencode = \
+            get_next_video_to_reencode(path_file_report)
+        if return_next_video_to_reencode is False:
+            logging.info('\nThere are no videos to reencode')
+            need_reencode = False
+            continue
+        
+        dict_video_data = return_next_video_to_reencode  
+        return_reencode_video = reencode_video(dict_video_data)
+        if return_reencode_video is False:
+            sys.exit()
+            return
+        
+        df = update_file_report(path_file_report, dict_video_data)
+        
+        # Save reports
+        df.to_excel(path_file_report, index=False)
+        create_report_backup(df=df, path_file_report=path_file_report, 
+                             tag='reencode')
+            
+    # sys.exit()
     return df
         
         
@@ -701,8 +862,8 @@ def step_create_report_filled(path_dir, path_file_report):
 
 def set_make_reencode(path_file_report):
 
-    df = pd.read_excel(path_file_report)
-    df = make_reencode(df)
+    
+    df = make_reencode(path_file_report)
     df.to_excel(path_file_report, index=False)
     
     #make backup
